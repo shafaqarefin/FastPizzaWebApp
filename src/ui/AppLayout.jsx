ac6553332a636f1265d4f61eda53d0ea4c6e8c02
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-14 03:52:44 +0600

    removed unecessary imports

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigation, useRouteError } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 import Error from "./Error";
@@ -7,7 +7,6 @@ import Loader from "./Loader";
 function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
-  const error = useRouteError();
   return (
     <div>
       {isLoading && <Loader />}
