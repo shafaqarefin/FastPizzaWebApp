e10fdd361fbbacba1adef2a9eb1d03ee5e0749f3
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-18 15:37:57 +0600

    Added new styling to menu component

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,16 +1,20 @@
-import { formatCurrency } from "../../utils/helpers";
+import Button from '../../ui/Button';
+import { formatCurrency } from '../../utils/helpers';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div>
+    <li className="mb-4 grid grid-cols-[auto_1fr] gap-8 border-b-2 border-gray-400 p-4">
+      <img src={imageUrl} alt={name} className="h-32 w-32" />
+      <div className="grid grid-rows-[auto_auto_auto]">
+        <p className="font-medium">{name}</p>
+        <p class="text-sm capitalize italic text-stone-500">
+          {ingredients.join(', ')}
+        </p>
+        <div class="flex flex-row items-center justify-between">
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+          <Button>Add to Cart</Button>
         </div>
       </div>
     </li>
