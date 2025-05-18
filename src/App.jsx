f0fb98f4e6d85e9db916404be385ae4d0f5dac99
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-18 15:36:11 +0600

    add errorElement to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,42 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
 import CreateOrder, {
   action as orderAction,
-} from "./features/order/CreateOrder";
-import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
-import { loader as menuLoader } from "./features/menu/Menu";
-import { loader as orderLoader } from "./features/order/Order";
+} from './features/order/CreateOrder';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
+import { loader as menuLoader } from './features/menu/Menu';
+import { loader as orderLoader } from './features/order/Order';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
       {
-        path: "/cart",
+        path: '/cart',
         element: <Cart />,
       },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: orderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
