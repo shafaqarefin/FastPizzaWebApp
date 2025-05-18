c9460055d982c9337e9600434ce0ecf3c0e42995
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-18 15:50:26 +0600

    Added new styling ,Username component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co</Link>
+    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-400 p-4 uppercase">
+      <Link to="/" className="text-xl tracking-widest">
+        Fast React Pizza Co
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
