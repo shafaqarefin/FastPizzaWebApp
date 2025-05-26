751aaf66f8f4978559554b31f4586348f35584d7
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-27 00:27:49 +0600

    Added commment

--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,8 +1,9 @@
 export async function getAddress({ latitude, longitude }) {
+  //receives lng and lat from getGeoLocation Promise and it fetches the speccific address from this API by passing in lat and lng which in turn returns the exact address
   const res = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
   );
-  if (!res.ok) throw Error("Failed getting address");
+  if (!res.ok) throw Error('Failed getting address');
 
   const data = await res.json();
   return data;
