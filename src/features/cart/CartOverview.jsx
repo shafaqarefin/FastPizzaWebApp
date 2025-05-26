1888815f766053bf9b42c4fbc47120c66a181172
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-27 00:22:31 +0600

    Now Displays total pizzas and price

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,18 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getTotalCartPrice, getCartQuanity } from './cartSlice';
 
 function CartOverview() {
+  const totalPizza = useSelector(getCartQuanity);
+  const totalPrice = useSelector(getTotalCartPrice);
+  if (totalPizza === 0) {
+    return;
+  }
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 text-sm uppercase text-stone-200 sm:p-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalPizza} pizzas</span>
+        <span>&#x20AC;{totalPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
