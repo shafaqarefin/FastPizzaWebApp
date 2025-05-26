a8afcd1e03c8de8d09d6fa68f475b02a118e7f8b
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-27 00:28:35 +0600

    New styling using tailwind,added add to cart,update and delete cart functionality and UI

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,20 +1,63 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addItem, getCartQuantityById } from '../cart/cartSlice';
+import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+  const currentQuantity = useSelector(getCartQuantityById(id));
+  const isInCart = currentQuantity > 0;
+
+  function handleAddtoCart() {
+    const orderItem = {
+      pizzaId: id,
+      name: name,
+      quantity: 1,
+      unitPrice: unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(orderItem));
+  }
 
   return (
-    <li className="mb-4 grid grid-cols-[auto_1fr] gap-8 border-b-2 border-gray-400 p-4">
-      <img src={imageUrl} alt={name} className="h-32 w-32" />
-      <div className="grid grid-rows-[auto_auto_auto]">
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? 'opacity-50 grayscale' : ''}`}
+      />
+      <div className="flex grow flex-col">
         <p className="font-medium">{name}</p>
-        <p class="text-sm capitalize italic text-stone-500">
+        <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(', ')}
         </p>
-        <div class="flex flex-row items-center justify-between">
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
-          <Button>Add to Cart</Button>
+        <div className="mt-auto flex items-center justify-between">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
+          {!soldOut && !isInCart && (
+            <Button type={'small'} onClick={handleAddtoCart}>
+              Add to Cart
+            </Button>
+          )}
+
+          {isInCart && !soldOut && (
+            <div className="flex items-center justify-between gap-3 sm:gap-8">
+              <DeleteItem pizzaId={id} />
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+            </div>
+          )}
         </div>
       </div>
     </li>
