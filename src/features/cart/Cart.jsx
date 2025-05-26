d7eae58884053841c7e357ff386440380d8dcb57
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-27 00:21:18 +0600

    Clear Cart Feature ,UI,EmptyCart Message conditonally shown

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,42 +1,40 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import CartItem from '../cart/CartItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserName } from '../user/userSlice';
+import { clearCart, getCart } from './cartSlice';
+import EmptyCart from '../../ui/EmptyCart';
 
 function Cart() {
-  const cart = fakeCart;
-
+  const cart = useSelector(getCart);
+  const username = useSelector(getUserName);
+  const dispatch = useDispatch();
+  function handleClearCart() {
+    dispatch(clearCart());
+  }
+  if (cart.length === 0) {
+    return <EmptyCart />;
+  }
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton to={'/menu'}>&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart,{username}</h2>
+      <ul className="mt-3 divide-y divide-stone-800 border-b border-stone-200">
+        {cart.map((cartItem) => (
+          <CartItem item={cartItem} key={cartItem.pizzaId} />
+        ))}
+      </ul>
 
-      <div>
-        <Button to="/order/new">Order pizzas</Button>
-        <button>Clear cart</button>
+      <div className="mt-6 space-x-8">
+        <Button type="primary" to="/order/new">
+          Order pizzas
+        </Button>
+
+        <Button type="secondary" onClick={handleClearCart}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
